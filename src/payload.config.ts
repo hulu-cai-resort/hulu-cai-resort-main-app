f4e02cfb964fa26c50b45fce150da01a58522545
genzo0 <m.alifyan02@gmail.com>
2025-09-04 12:52:55 +0700

    remove prod migrations

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -37,7 +37,6 @@ import { EventsOutdoorPage } from './globals/EventsOutdoorPage/config'
 import { ReservationFormPage } from './globals/ReservationFormPage/config'
 import { ContactForm } from './globals/ContactForm'
 import { MapMarkers } from './collections/MapMarkers'
-import { migrations } from './migrations'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -84,7 +83,6 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
-    prodMigrations: migrations,
   }),
   collections: [
     Accommodations,
